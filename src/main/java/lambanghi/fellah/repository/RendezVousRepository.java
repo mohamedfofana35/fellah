4383a3f498e06eb77515e5d48b19c6f76b812bb5
mohamedfofana35 <Rennes@2060>
2025-08-20 16:25:28 +0200

    feat(repository): implémentation complète des repository

--- src/main/java/lambanghi/fellah/repository/RendezVousRepository.java
+++ src/main/java/lambanghi/fellah/repository/RendezVousRepository.java
@@ -2,10 +2,9 @@ package lambanghi.fellah.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import lambanghi.fellah.entity.Medecin;
 import lambanghi.fellah.entity.RendezVous;
 
 public interface RendezVousRepository extends JpaRepository<RendezVous, Long> {
-    boolean existsByMedecinAndDateHeure(Medecin medecin, java.time.LocalDateTime dateHeure);
+//    boolean existsByMedecinAndDateHeure(Medecin medecin, java.time.LocalDateTime dateHeure);
 
 }
