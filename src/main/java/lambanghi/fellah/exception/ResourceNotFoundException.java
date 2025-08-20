4e9c10214d5e421bed32b15d7fd1f35ec1dd2d92
mohamedfofana35 <Rennes@2060>
2025-08-20 16:20:14 +0200

    feat(exception): implémentation  des exception

---
+++ src/main/java/lambanghi/fellah/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package lambanghi.fellah.exception;
+
+
+@SuppressWarnings("serial")
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
