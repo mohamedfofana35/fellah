06d32ac393da6239b92a947f50b2c6e190607947
mohamedfofana35 <Rennes@2060>
2025-08-28 19:56:08 +0200

    refactor(exception):  personnalisation et correction mineure  de la classe d’exception pour la gestion des erreurs

--- src/main/java/lambanghi/fellah/exception/ResourceNotFoundException.java
+++ src/main/java/lambanghi/fellah/exception/ResourceNotFoundException.java
@@ -1,7 +1,5 @@
 package lambanghi.fellah.exception;
 
-
-@SuppressWarnings("serial")
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
