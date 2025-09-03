a67b552034daa197763d41834a3d5500c063d2d9
mohamedfofana35 <Rennes@2060>
2025-09-03 20:25:07 +0200

    config: ajout de SwaggerConfig pour personnaliser la documentation OpenAPI

---
+++ src/main/java/lambanghi/fellah/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package lambanghi.fellah.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Clinique Fellah")
+                        .description("Documentation des endpoints de l'application de gestion de la clinique Fellah")
+                        .version("1.0.0"));
+    }
+}
+
+    
+
